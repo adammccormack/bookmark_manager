959813e83b6adcb0b55398da25a14eb0f13e559a
Adam <adam.j.mccor@gmail.com>
2021-04-30 17:29:08 +0100

    'app update'

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require './lib/bookmark'
+require './database_connection_setup'
+
+class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Bookmark Manager'
+    erb :index
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :bookmarks
+  end
+
+  get '/bookmarks/new' do
+    erb :new
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.get(id: params[:id])
+    erb :edit
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  run! if app_file == $0
+end
